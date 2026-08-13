1713e44a6ceaa82f535fa7891255c3c6197924de
ThorBeck20 <thorbeck15@gmail.com>
2026-08-12 18:24:52 -0700

    Updated .gitignore and fixed a lint bug.

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from Backend.routers import bonds
+from fastapi import FastAPI  # type: ignore
 
 app = FastAPI()
 
+app.include_router(bonds.router)
+
+
 @app.get("/health")
 def health():
-	return {"status" : "ok"}
-
-
+    return {"status": "ok"}
